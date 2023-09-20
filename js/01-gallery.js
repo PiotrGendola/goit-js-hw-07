42fe7cb164aead041af2c2e332fca82f1694ff7f
Piotr Gendola <piotrgendola@gmail.com>
2023-09-20 23:57:51 +0200

    hw07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imagesList = document.querySelector("ul.gallery");
+const galleryCards = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__link" href=${original}>
+<img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+/>
+</a>
+</div>`;
+    })
+    .join("");
+}
+imagesList.insertAdjacentHTML("beforeend", galleryCards);
+imagesList.addEventListener("click", galleryElement);
+
+function galleryElement(event) {
+  event.preventDefault();
+  basicLightbox
+    .create(
+      `
+  <img width="800" height="600" src=${event.target.dataset.source}>
+`
+    )
+    .show();
+}
